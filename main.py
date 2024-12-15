a89b95f29e74a0dc17c2675a28d087f73ce3a72a
Angel-A15 <aasupertwix15@gmail.com>
2024-12-15 04:19:14 -0600

    established the enability to generate questions randomly on the terminal, working on user input

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ def load_questions():
 
     return questions
 
+def get_random_questons(questions, num_questions):
+    if num_questions > len(questions):
+        num_questions = len(questions)
+
+    random_questions = random.sample(questions, num_questions)
+    return random_questions
+
+
 questions = load_questions()
-print(questions)
+random_queestions = get_random_questons(questions, 1)
+print(random_queestions)
